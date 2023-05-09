1441b54284c93d30a8dcb540c5d551eae1e4fcd8
Thomas Notarangelo <thomas@macbook-pro-de-thomas.home>
2023-05-09 23:38:09 +0200

    signup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 // Import de mes packages
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cookies from "js-cookie";
 
 // Import de mes Pages
 import Home from "./pages/Home";
@@ -13,15 +14,29 @@ import Login from "./pages/Login";
 import Header from "./components/Header";
 
 function App() {
+  // Je créer une fonction pour enregistrer le token dans les cookies si mon argument est définit
+  const handleToken = (token) => {
+    if (token) {
+      Cookies.set("userToken", token, { expires: 10 });
+    } else {
+      Cookies.remove("userToken");
+    }
+  };
   return (
     <Router>
-      <Header />
+      <Header handleToken={handleToken} />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         {/* Je définie que ce qui sera après offer sera l'id */}
         <Route path="/offer/:id" element={<Offer />}></Route>
-        <Route path="/signup" element={<Signup />}></Route>
-        <Route path="/login" element={<Login />}></Route>
+        <Route
+          path="/signup"
+          element={<Signup handleToken={handleToken} />}
+        ></Route>
+        <Route
+          path="/login"
+          element={<Login handleToken={handleToken} />}
+        ></Route>
       </Routes>
     </Router>
   );
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,13 @@
-const Signup = () => {
-  return <div>S'inscrire</div>;
+const Signup = ({ handleToken }) => {
+  return (
+    <form className="form-container">
+      <h1>S'inscrire</h1>
+      <input type="text" placeholder="Username" />
+      <input type="text" placeholder="Email" />
+      <input type="text" placeholder="Password" />
+      <input type="checkbox" />
+      <button type="submit"> S'inscrire </button>
+    </form>
+  );
 };
 export default Signup;
